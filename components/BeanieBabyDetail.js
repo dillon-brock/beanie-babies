9e54d0d5652eaca6ed94427c9c77ef1bf030854c
Dillon Brock <mdillon1297@gmail.com>
2022-06-22 14:49:20 -0700

    rainbow table!

--- components/BeanieBabyDetail.js
+++ components/BeanieBabyDetail.js
@@ -8,16 +8,42 @@ export default function createBeanieBabyDetail(root) {
 
         image.src = beanieBaby.image;
         name.textContent = beanieBaby.title;
-        for (const [k, v] of Object.entries(beanieBaby)) {
-            tbody.append(tableRow(k, v));
+        // name.style.backgroundColor = 'hsl(360, 50%, 40%)';
+        let numOfRows = 0;
+        for (const v of Object.values(beanieBaby)) {
+            if (v && v !== 'N/A' && v !== '-') {
+                numOfRows++;
+            }
         }
+        let rowNum = 0;
+        for (const [k, v] of Object.entries(beanieBaby)) {
+            if (v && v !== 'N/A' && v !== '-') {
+                const tr = tableRow(k, v);
+                tbody.append(tr);
+            }
+        }
+
+        const thElements = tbody.querySelectorAll('th');
+        const tdElements = root.querySelectorAll('td');
+        thElements.forEach(th => {
+            let h = rowNum * (360 / numOfRows);
+            th.style.backgroundColor = `hsl(${h}, 80%, 60%)`;
+            rowNum++;
+        });
+
+        rowNum = 0;
+        tdElements.forEach(td => {
+            let h = rowNum * (360 / numOfRows);
+            td.style.border = `5px solid hsl(${h}, 80%, 60%)`;
+            rowNum++;
+        });
     };
 }
 
 function tableRow(k, v) {
     const tr = document.createElement('tr');
 
-    const tk = document.createElement('td');
+    const tk = document.createElement('th');
     tk.classList.add('key');
     tk.textContent = k;
 
