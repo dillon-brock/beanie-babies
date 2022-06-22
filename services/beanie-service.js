711d4852c2f820c58b38af5a879a4fee9e36f05c
Dillon Brock <mdillon1297@gmail.com>
2022-06-22 09:13:55 -0700

    built out fetch functions

---
+++ services/beanie-service.js
@@ -0,0 +1,26 @@
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getBeanieBaby(id) {
+    const response = client
+        .from('beanie_babies')
+        .select(`
+                id,
+                image,
+                astroSign,
+                title
+                `)
+        .match({ id })
+        .single();
+    
+    return response.data;
+}
+
+export async function getBeanieBabies() {
+    const response = client
+        .from('beanie_babies')
+        .select();
+    
+    return response.data;
+}
